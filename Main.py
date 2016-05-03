cba6d2e0bff351d3e988b8fe2f871a1457fc68e5
vampiroDx <vA6corporation@gmail.com>
2016-05-03 14:10:44 -0500

    Agregamos el boton menos

--- Main.py
+++ Main.py
@@ -52,8 +52,8 @@ boton1 = Button(text='=', font=('ubuntu',24))
 boton1.place(height=40, width=85,x=55,y=205)
 
 #Botones de operaciones matematicas
-boton1 = Button(text='+', font=('ubuntu',24))
-boton1.place(height=85, width=40,x=145,y=160)
+boton1 = Button(text='-', font=('ubuntu',24))
+boton1.place(height=85, width=40,x=145,y=70)
 
 boton1 = Button(text='+', font=('ubuntu',24))
 boton1.place(height=85, width=40,x=145,y=160)
