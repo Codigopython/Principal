90890215452f8fc47ac397e1de3afdb30b344156
vampiroDx <vA6corporation@gmail.com>
2016-05-04 22:17:35 -0500

    Funcionalidad completa de los numeros y de las operaciones suma y resta :)

--- Main.py
+++ Main.py
@@ -7,7 +7,6 @@ gui.geometry('200x300+2000+1')
 gui.config(bg='white')
 
 #Caja de operaciones
-
 lista1 = Listbox(bd=0,height=3)
 lista1.pack()
 
@@ -15,59 +14,66 @@ caja1 = Entry(bd=0)
 caja1.pack()
 
 #botones del 0 al 9
-boton1 = Button(text='7', font=('ubuntu',24))
+boton1 = Button(bd=0, text='7', font=('ubuntu',24),
+command=lambda: caja1.insert(END,'7'))
 boton1.place(height=40, width=40,x=10,y=70)
 
-boton1 = Button(text='8', font=('ubuntu',24))
+boton1 = Button(bd=0, text='8', font=('ubuntu',24),
+command=lambda: caja1.insert(END,'8'))
 boton1.place(height=40, width=40,x=55,y=70)
 
-boton1 = Button(text='9', font=('ubuntu',24))
+boton1 = Button(bd=0, text='9', font=('ubuntu',24),
+command=lambda: caja1.insert(END,'9'))
 boton1.place(height=40, width=40,x=100,y=70)
 
-boton1 = Button(text='4', font=('ubuntu',24))
+boton1 = Button(bd=0, text='4', font=('ubuntu',24),
+command=lambda: caja1.insert(END,'4'))
 boton1.place(height=40, width=40,x=10,y=115)
 
-boton1 = Button(text='5', font=('ubuntu',24))
+boton1 = Button(bd=0, text='5', font=('ubuntu',24),
+command=lambda: caja1.insert(END,'5'))
 boton1.place(height=40, width=40,x=55,y=115)
 
-boton1 = Button(text='6', font=('ubuntu',24))
+boton1 = Button(bd=0, text='6', font=('ubuntu',24),
+command=lambda: caja1.insert(END,'6'))
 boton1.place(height=40, width=40,x=100,y=115)
 
-boton1 = Button(text='3', font=('ubuntu',24))
+boton1 = Button(bd=0, text='3', font=('ubuntu',24),
+command=lambda: caja1.insert(END,'3'))
 boton1.place(height=40, width=40,x=100,y=160)
 
-boton1 = Button(text='2', font=('ubuntu',24))
+boton1 = Button(bd=0, text='2', font=('ubuntu',24),
+command=lambda: caja1.insert(END,'2'))
 boton1.place(height=40, width=40,x=55,y=160)
 
-boton1 = Button(text='1', font=('ubuntu',24),
+boton1 = Button(bd=0, text='1', font=('ubuntu',24),
 command=lambda: caja1.insert(END,'1'))
 boton1.place(height=40, width=40,x=10,y=160)
 
-boton1 = Button(text='0', font=('ubuntu',24),
+boton1 = Button(bd=0, text='0', font=('ubuntu',24),
 command=lambda: caja1.insert(END,'0'))
 boton1.place(height=40, width=40,x=10,y=205)
 
-#Boton Clear
-boton1 = Button(text='CE', font=('ubuntu',20),
-command=lambda: (caja1.delete(0,END), lista1.delete(0,END)))
-boton1.place(height=40, width=40,x=100,y=205)
-
 #Botones de operaciones matematicas
-boton1 = Button(text='-', font=('ubuntu',24),
-command=lambda: Captura2(int(caja1.get())))
+boton1 = Button(bd=0, text='-', font=('ubuntu',24),
+command=lambda: (Operacion("resta"), lista1.insert(END,
+Captura1(int(caja1.get()))),caja1.delete(0,END)))
 boton1.place(height=85, width=40,x=145,y=70)
 
-boton1 = Button(text='+', font=('ubuntu',24),
-command=lambda: (lista1.insert(END, Captura1(int(caja1.get()))),
-caja1.delete(0,END)))
+boton1 = Button(bd=0, text='+', font=('ubuntu',24),
+command=lambda: (Operacion("suma"), lista1.insert(END,
+Captura1(int(caja1.get()))), caja1.delete(0,END)))
 boton1.place(height=85, width=40,x=145,y=160)
 
-boton1 = Button(text='=', font=('ubuntu',24),
+#Boton igual
+boton1 = Button(bd=0, text='=', font=('ubuntu',24),
 command=lambda: (lista1.insert(END, Captura2(int(caja1.get())),
-caja1.delete(0,END)), lista1.insert(END, Suma())))
+caja1.delete(0,END)), lista1.insert(END, Calculo())))
 boton1.place(height=40, width=40,x=55,y=205)
 
-# button1.place(bordermode=OUTSIDE, height=40, width=40, y=70)
-
+#Boton Clear
+boton1 = Button(bd=0, text='CE', font=('ubuntu',20),
+command=lambda: (caja1.delete(0,END), lista1.delete(0,END)))
+boton1.place(height=40, width=40,x=100,y=205)
 
 gui.mainloop()
--- Modulos.py
+++ Modulos.py
@@ -1,18 +1,28 @@
+def Operacion(a):
+    global operacion
+    operacion = a
+    print(operacion)
+
 def Captura1(num1):
     global dato1
     dato1 = num1
     print("captura1>> ", dato1)
     return dato1
 
-
 def Captura2(num2):
     global dato2
     dato2 = num2
-    print ("captura2>> ", dato2)
+    print("captura2>> ", dato2)
     return dato2
 
-def Suma():
-    operacion = dato1 + dato2
-    print ("resulta>> ",operacion)
-    return operacion
-#     return operacion
+def Calculo():
+    if operacion == "suma":
+        suma = dato1 + dato2
+        print("operacion>> ",suma)
+        return suma
+    elif operacion == "resta":
+        resta = dato1 - dato2
+        print("operacion>> ", resta)
+        return resta
+    else:
+        print("algo esta mal")
